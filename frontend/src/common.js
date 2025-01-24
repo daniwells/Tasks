c02a20e4f9f7b5d2a62f051cee9a827e4d39bbfc
Daniel Lima <130805695+daniwells@users.noreply.github.com>
2025-01-23 21:41:00 -0300

    :wastebasket: chore: Escondendo IP do servidor

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -1,8 +1,6 @@
 import { Alert, Platform } from "react-native";
 
-const server = Platform.OS === "ios"
-    ? "http://192.168.0.181:3000" : "http://192.168.0.181:3000";
-    // ? "http://10.3.77.97:3000" : "http://10.3.77.97:3000";
+const server = "<ip_server>:3000";
     
 function showError(err) {
     if(err.response && err.response.data){
